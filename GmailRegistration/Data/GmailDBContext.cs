0ba7de66e1565468685a41b01b905327f3bf2f25
Harsh Singh <kmrharsh50@gmail.com>
2024-10-03 14:29:27 +0530

    Harsh[Added] : Adding Users table to the DB

---
+++ GmailRegistration/Data/GmailDBContext.cs
@@ -0,0 +1,24 @@
+﻿using GmailRegistration.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GmailRegistration.Data
+{
+    // Database context for the GmailRegistrationDemo application.
+    public class GmailDBContext : DbContext
+    {
+        public GmailDBContext(DbContextOptions<GmailDBContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure Gender to be stored as string
+            modelBuilder.Entity<User>()
+                .Property(u => u.Gender)
+                .HasConversion<string>()
+                .IsRequired();
+        }
+        // DbSet representing the Users table in the database.
+        public DbSet<User> Users { get; set; }
+    }
+}
